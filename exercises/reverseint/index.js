53b94ae702c4dfd7d2ec63bb120799077b4eae43
Gourab <smlgourab@gmail.com>
2024-08-18 13:09:53 +0530

    Commit

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // first way
+  //   let sign = Math.sign(n);
+  //   let nAsString = sign === -1 ? n.toString().trimStart("-") : n.toString();
+  //   let reverseString = nAsString.split("").reverse().join("").trimStart("0");
+
+  //   return sign === -1 ? parseInt(`-${reverseString}`) : parseInt(reverseString);
+
+  //second way
+  return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
+}
 
 module.exports = reverseInt;
