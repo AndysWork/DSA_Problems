236535abc64caade49a9fa963ab03db6876f7291
Gourab <smlgourab@gmail.com>
2024-08-22 23:09:24 +0530

    Steps Problems Commit

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Solution One
+
+// function pyramid(n) {
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let stairs = "";
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       stairs +=
+//         midPoint - row <= column && midPoint + row >= column ? "#" : " ";
+//     }
+//     console.log(stairs);
+//   }
+// }
+
+//Solution Two - Recursion
+
+function pyramid(n, row = 0, level = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (2 * n - 1 === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1, "");
+  }
+
+  const midPoint = Math.floor((2 * n - 1) / 2);
+  const add =
+    midPoint - row <= level.length && midPoint + row >= level.length
+      ? "#"
+      : " ";
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
