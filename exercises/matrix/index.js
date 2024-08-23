10e477710b83d55870fa7467266f511c06c17144
Gourab <smlgourab@gmail.com>
2024-08-23 19:57:18 +0530

    Spiral Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,44 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [];
+  let counter = 1;
+  let start_col = 0;
+  let start_row = 0;
+  let end_col = n - 1;
+  let end_row = n - 1;
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  while (start_col <= end_col && start_row <= end_row) {
+    // Start Row
+    for (let i = start_col; i <= end_col; i++) {
+      results[start_row][i] = counter++;
+    }
+    start_row++;
+
+    ///Right_Column
+    for (let i = start_row; i <= end_row; i++) {
+      results[i][end_col] = counter++;
+    }
+    end_col--;
+
+    // Bottom Row
+    for (let i = end_col; i >= start_col; i--) {
+      results[end_row][i] = counter++;
+    }
+    end_row--;
+
+    //StartColomn
+    for (let i = end_row; i >= start_row; i--) {
+      results[i][start_col] = counter++;
+    }
+    start_col++;
+  }
+  return results;
+}
 
 module.exports = matrix;
